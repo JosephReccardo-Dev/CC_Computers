2bc6b87f363d5872516c08921b821a1cf881ed21
Joseph Reccardo <135245920+JosephReccardo-Dev@users.noreply.github.com>
2023-08-21 09:33:19 -0400

    Add files via upload

---
+++ cc_computers/cc_contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact Us</title>
+</head>
+<body>
+	<?php include("cc_header.php"); ?>
+
+	<h2 style="color: orange; font-size: 2.5em">Contact Us Today</h2>
+	<br/>
+	<p>Coast City Computers</p>
+	<address>
+		3601 Hempstead Turnpike<br/>
+		Levittown, NY 11756<br/>
+		(516) 796-1000<br/>
+		Customer.support@ccc.com<br/>
+	</address>
+
+	<a href="cc_home.php">Let's Go Home</a>
+
+	<?php include("cc_footer.php"); ?>
+</body>
+</html>
